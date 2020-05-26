661ee2767910346a28b159fe98931342f095c748
Nick-or-lollo <65964439+Nick-or-lollo@users.noreply.github.com>
2020-05-26 17:14:21 +0200

    Add files via upload

---
+++ EchoServerTest.java
@@ -0,0 +1,50 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class EchoServerTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class EchoServerTest
+{
+    /**
+     * Default constructor for test class EchoServerTest
+     */
+    public EchoServerTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testPORT()
+    {
+        EchoServer echoServ1 = new EchoServer();
+        assertEquals(50000, echoServ1.getPORT());
+    }
+}
+
